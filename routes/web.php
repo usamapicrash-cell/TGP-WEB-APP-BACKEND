4be7e01a28c6f80b65f710adcb9947c3c75fd101
Usama <usamapicrash@gmail.com>
2026-07-30 06:37:48 -0700

    fixed

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,34 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/storage/job-media/{filename}', function ($filename) {
+    $path = 'job-media/' . $filename;
+
+    if (!Storage::disk('public')->exists($path)) {
+        abort(404);
+    }
+
+    return Storage::disk('public')->response($path);
+});
+
+
+
+Route::get('/clear-all-cache', function () {
+    Artisan::call('config:clear');
+    Artisan::call('cache:clear');
+    Artisan::call('view:clear');
+    Artisan::call('route:clear');
+
+    return response()->json([
+        'status' => 'success',
+        'message' => 'Config, Cache, View, and Route successfully cleared!'
+    ]);
 });
